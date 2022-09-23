2bd0634dfa4d6e18d71367b895ce34820fcbafd1
Kósa Balázs <emberbencsface@gmail.com>
2022-09-23 13:52:31 +0200

    index.js - operátorok implementálása, bugfixek

--- src/index.js
+++ src/index.js
@@ -15,17 +15,34 @@ const CLEAR_ENTRY = document.getElementById("clear-entry");
 const PI_ = document.getElementById("pi");
 const EULER = document.getElementById("euler");
 
+const equalsBtn = document.getElementById("equals");
 const pi = Math.PI.toFixed(5);
 
 const RESULT = document.getElementById("results");
-let wasConstantClicked = false;
+const PREVIOUS_ENTRY = document.getElementById("previous-entry");
 
+let wasConstantClicked = false;
+let currentOperation = "";
+let computation_finished = false;
+let beforeComputation = false;
 
 const numbers = document.getElementsByClassName("number");
 Array.from(numbers).forEach(
     number =>{
         number.addEventListener('click',
         ()=>{
+            if(computation_finished){
+                clearDisplay();
+                clearCurrentInput();
+                resetVariables();
+            }
+            
+            if(beforeComputation){
+                clearCurrentInput();
+                beforeComputation = false;
+            }
+            
+
             if(RESULT.innerText === '0'){
                 if(number.innerText === '0')
                     return
@@ -34,29 +51,116 @@ Array.from(numbers).forEach(
                     RESULT.innerText = number.innerText;
                 }
             }else{
-                if(wasConstantClicked){
+                if(wasConstantClicked || computation_finished){
                     RESULT.innerText = number.innerText;
                     wasConstantClicked = false;
                 }
                 else
                     RESULT.innerText += number.innerText;
             }
+        computation_finished = false;
         });
     }
 );
 
+function clearDisplay(){
+    console.log("clearing the display");
+    PREVIOUS_ENTRY.innerText = "";
+    computation_finished = false;
+    currentOperation = '';
+}
+
+function resetVariables(){
+    wasConstantClicked = false;
+    currentOperation = "";
+    computation_finished = false;
+    beforeComputation = false;
+}
+
+function clearCurrentInput(){
+    RESULT.innerText = "";
+}
 
 
+const operations = document.getElementsByClassName("operator");
+
+Array.from(operations).forEach(operation =>{
+    operation.addEventListener("click",(e)=>{
+    currentOperation = operation.innerText;
+    PREVIOUS_ENTRY.innerText = RESULT.innerText+" "+ currentOperation+ " ";
+    beforeComputation = true;
+    computation_finished = false;
+    console.log(currentOperation);
+    });
+})
+
+equalsBtn.addEventListener("click",()=>{
+    console.log("clicked")
+    applyOperation();
+})
+
+const applyOperation = () =>{
+    if(currentOperation === ""){
+        console.log("returning")
+        return;
+    }
+    let computation = 0;
+
+    let prev = parseFloat(PREVIOUS_ENTRY.innerText)
+    let current = parseFloat(RESULT.innerText)
+
+    if(computation_finished){
+        prev = parseFloat(RESULT.innerText);
+        current = parseFloat(PREVIOUS_ENTRY.innerText.split(/[\s,]+/)[2]);
+        console.log(`${prev} ${current}`);
+    }
+
+    switch (currentOperation) {
+        case '+':
+            computation = prev + current
+            break
+        case '-':
+            computation = prev - current
+            break
+        case '*':
+            computation = prev * current
+            break
+        case '/':
+            computation = prev / current
+            break
+        default:
+            return
+        }
+    
+        if(computation_finished){
+            PREVIOUS_ENTRY.innerText = `${prev} ${currentOperation} ${current} =`;
+            RESULT.innerText = computation.toString();
+            return;
+        }
+        PREVIOUS_ENTRY.innerText +=" " + RESULT.innerText + " " + "="; 
+        RESULT.innerText = computation.toString();
+        computation_finished = true;
+//TODO: eredmény elmentése az előzményekbe        
+}
 
 CLEAR_ENTRY.addEventListener("click",()=>{
     RESULT.innerText = "0";
+    if(computation_finished){
+        PREVIOUS_ENTRY.innerText = "";
+        resetVariables();
+    }
 })
 
 CLEAR.addEventListener("click",()=>{
     RESULT.innerText = "0";
+    PREVIOUS_ENTRY.innerText = "";
+    resetVariables();
 })
 
 DEL.addEventListener("click",()=>{
+    if(wasConstantClicked){
+        return;
+    }
     const old_len = RESULT.innerText.length;
     if(old_len -1== 0){
         RESULT.innerText = "0";
